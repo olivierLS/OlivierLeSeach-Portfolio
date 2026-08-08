ccd3e7a75ed0d703870087c9afac34665197229e
olivierLS <olivierleseach@live.fr>
2026-08-08 15:12:51 +0200

    Change developer name in footer
    Updated footer text to reflect the correct developer's name.

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -5,7 +5,7 @@
   ft.innerHTML = `
     <div class="footer">
       <p>© ${year} — Olivier Le SEAC'H • BTS SIO</p>
-      <p class="muted">Site statique (HTML/CSS/JS). Aucune dépendance externe. Développé par Maxime GOUSSU</p>
+      <p class="muted">Site statique (HTML/CSS/JS). Aucune dépendance externe. Développé par Olivier Le Seac'h</p>
       <p class="footer__links">
         <a href="mentions-legales.html">Mentions légales</a> •
         <a href="confidentialite.html">Politique de confidentialité</a>
